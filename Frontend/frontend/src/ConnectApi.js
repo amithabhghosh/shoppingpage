ce801fbf2f25e48b70abb4d047a1a179cf7b7f8c
amithabhghosh <amithabhghosh@gmail.com>
2025-01-31 21:07:49 +0530

    credintial true

--- Frontend/frontend/src/ConnectApi.js
+++ Frontend/frontend/src/ConnectApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 const API = axios.create({
     baseURL: "https://shop-backend-sooty.vercel.app",
-    withCredentials: false, // Change to true only if you’re using credentials
+    withCredentials: true, // Change to true only if you’re using credentials
 });
 export default API
